8692600431378f1c071e67c2215cd90983a572d4
zhl <861805366@qq.com>
2019-12-04 18:53:09 +0800

    写首页特效

---
+++ src/views/index/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="page-index clearFix">
+        <template v-for="(yItem, yIndex) in canSeeHeight">
+            <template v-for="(xItem, xIndex) in canSeeWidth">
+                <div  :key="xIndex + '-' + yIndex" :style="assignedBackgroundImage(xIndex, yIndex)" class="fl"></div>
+            </template>
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'index',
+    data () {
+        return {
+            canSeeHeight: 0,
+            canSeeWidth: 0
+        }
+    },
+    mounted () {
+        this.canSeeHeight = document.body.clientHeight / 10;
+        this.canSeeWidth = document.body.clientWidth / 10;
+    },
+    methods: {
+        assignedBackgroundImage (xLocation, yLocation) {
+            return 'background: url("../../../static/images/bzh.jpg") no-repeat ' + (xLocation * -10 + 'px ') + (yLocation * -10 + 'px')
+        }
+    }
+}
+</script>
+
+<style scoped>
+.page-index {
+    width: 100vw;
+    height: 100vh;
+    /* background-image: url('../../../static/images/bzh.jpg'); */
+}
+.page-index .fl {
+    width: 10px;
+    height: 10px;
+}
+</style>
